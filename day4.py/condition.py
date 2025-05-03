d7a60f8b6e7f7b0519e0506cfc8c63ebcefd1638
Poojasri1429 <poojasri1429@gmail.com>
2025-05-03 17:34:31 +0530

    Add files via upload

---
+++ day4.py/condition.py
@@ -0,0 +1,31 @@
+# age = 14
+# if age >= 18:
+#     print("You are an adult.")
+# else:
+#     print("You are not an adult.")
+
+
+
+# marks = int(input("Enter your marks: "))
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 75:
+#     print("Grade: B")
+# elif marks >= 60:
+#     print("Grade: C")
+# else:
+#     print("Grade: F")
+
+
+# "Child" if age < 13
+# "Teen" if age is between 13 and 19
+# "Adult" if age is 20 or more
+
+
+age = int(input("Enter your age: "))
+if age <= 13:
+    print("Child")
+elif age >= 13 and age <= 19:
+    print("Teen")
+else :
+    print("Adult")
