07ed047992003fe784f76f703b5a967ec300e60c
Poojasri1429 <poojasri1429@gmail.com>
2025-05-12 19:48:04 +0530

    Add files via upload

---
+++ day12/greet.py
@@ -0,0 +1,5 @@
+def say_hello(name):
+    return f"Hello, {name}!"
+
+import greet
+print(greet.say_hello("Tara"))
---
+++ day12/module.py
@@ -0,0 +1,17 @@
+#module
+# import math
+# print(math.sqrt(16))  
+
+#improting module
+# import random
+# print(random.randint(1, 10))
+
+
+#Import specific function
+# from math import sqrt
+# print(sqrt(25))
+
+#Rename while importing
+# import math as m
+# print(m.pi)
+
